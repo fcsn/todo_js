c91a8e917e7ac8a78e13494fea03cf9581c30d7c
kitchenevan <sirfcsn@gmail.com>
2018-02-06 15:30:22 +0900

    add removeItem and completeItem in todo.js update README.md

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # javascript_todoApp
 
-자바스크립트를 학습하기 위한 예제 입니다. 2월 5일(월)에 시작하여 2월 10일(금)에 끝낼 예정입니다.
+자바스크립트를 학습하기 위한 예제 입니다. 2018년 2월 5일(월)에 시작하여 2월 10일(금)에 끝낼 예정입니다.
 
 ## 참고 자료
 
 * [Learn to code a to-do list app in JavaScript](https://www.youtube.com/watch?v=2wCpkOk2uCg&t=1334s) - 참고한 유튜브 동영상 입니다.
-* [themaxsandelin의 github](https://github.com/themaxsandelin/todo) - 유튜브 저자의 github 입니다.
+* [themaxsandelin의 github](https://github.com/themaxsandelin/todo) - 위 항목의 유튜브 저자의 github 입니다.
--- js/todo.js
+++ js/todo.js
@@ -4,12 +4,31 @@ var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:x
 
 
 
-document.getElementById('add').addEventListener('click', function(){
+document.getElementById('add').addEventListener('click', function() {
   var value = document.getElementById('item').value;
     if(value)
       addItemTodo(value);
+      document.getElementById('item').value = '';
 
 });
+
+function removeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+
+  parent.removeChild(item);
+}
+
+function completeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  var id = parent.id;
+
+  var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+
+  parent.removeChild(item);
+  target.insertBefore(item, target.childNodes[0]);
+}
 
 function addItemTodo(text) {
   var list = document.getElementById('todo');
@@ -24,13 +43,17 @@ function addItemTodo(text) {
   remove.classList.add('remove');
   remove.innerHTML = removeSVG
 
+  remove.addEventListener('click', removeItem);
+
   var complete = document.createElement('button');
   complete.classList.add('complete');
   complete.innerHTML = completeSVG
+
+  complete.addEventListener('click', completeItem);
 
   buttons.appendChild(remove);
   buttons.appendChild(complete);
   item.appendChild(buttons);
 
-  list.appendChild(item);
+  list.insertBefore(item, list.childNodes[0]);
 }
